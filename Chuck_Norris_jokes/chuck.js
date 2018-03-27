a11a5dfeb740010a13f5fcd5367bca35d4e2dfce
syahmiazril <37425817+syahmiazril@users.noreply.github.com>
2018-03-27 15:46:40 +0800

    Delete chuck.js

--- Chuck_Norris_jokes/chuck.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello')
-
-fetch('https://api.chucknorris.io/jokes/random?category=movie')
-.then(function (response){
-	return response.json();
-}).then(function(json){
-	const para = document.getElementById('jokes');
-	para.innerHTML = json.value
-})
